9ac7aa5f1ae2cd46c5b5544c9240d8f336e98d48
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-11-23 12:58:36 +0900

    initial commit

---
+++ OpenJiroMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenJiroMap
+//
+//  Created by 清 貴幸 on 2017/11/23.
+//  Copyright © 2017年 Takayuki Sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
