000ea1a9d8607355a65c77b3819e4455e24a72e1
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-12-18 21:47:19 +0900

    カスタムセルを追加

---
+++ OpenJiroMap/JiroCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  JiroCollectionViewCell.swift
+//  OpenJiroMap
+//
+//  Created by 清 貴幸 on 2017/12/18.
+//  Copyright © 2017年 Takayuki Sei. All rights reserved.
+//
+
+import UIKit
+
+class JiroCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var openLabel: UILabel!
+    @IBOutlet weak var openTimeLabel: UILabel!
+    @IBOutlet weak var thumbnail: UIImageView!
+}
